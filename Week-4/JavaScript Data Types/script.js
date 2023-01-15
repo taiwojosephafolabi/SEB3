994c0befc808044959a573a4194bdf29c0356fd4
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-15 18:45:07 +0000

    data types

---
+++ Week-4/JavaScript Data Types/script.js
@@ -0,0 +1,33 @@
+// NaN Exercise
+let a = "Hello";
+let b = "World";
+let res = a * b;
+alert(res);
+console.log(res);
+
+// Infinity and -Infinity
+alert(10 / 0);
+alert(-10 / 0);
+console.log(alert(10 / 0));
+console.log(alert(-10 / 0));
+
+// Variables
+let myFavouriteNumber = 3;
+let num1 = 27;
+let num2 = 500;
+let num3 = 23.44;
+console.log(myFavouriteNumber);
+console.log(num1, num2, num3);
+
+// Data Types
+console.log(typeof(3));
+console.log(typeof("hello world"));
+console.log(typeof(true));
+console.log(typeof((myObject = { function: "hello", function2: "world" })));
+let myInfinity = (10 / 0); 
+console.log(typeof(myInfinity));
+let myNull = null;
+console.log(typeof(myNull));
+
+
+alert(errorVariable);
