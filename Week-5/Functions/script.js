a6a9d0d081c2808b2b555a1d0c436e51f68ee663
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-16 20:07:08 +0000

    functions

---
+++ Week-5/Functions/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function multiply(number1, number2) {
+    return number1 * number2;
+};
+
+console.log(multiply(30, 4));
+
+function add(number1, number2, number3) {
+    return number1 + number2 + number3;
+}
+
+console.log(add(20, 30, 40));
+
+function divide(number1, number2){
+    let result = number1 / number2;
+    return result;
+}
+
+console.log(divide(100, 4));
+
+let result = divide(60, 4);
+console.log(result);
