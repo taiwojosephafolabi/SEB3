a0e8f4c50133e04cd77fc63f23285e61e7c5c698
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-17 18:48:20 +0000

    Objects

--- Week-5/Objects/script.js
+++ Week-5/Objects/script.js
@@ -16,13 +16,13 @@ const person = {
     flatNumber: 1,
     streetName: "London Road",
   },
-  role: ["student", "engineer"]
+  role: ["student", "engineer"],
 };
 
 console.log(person["myName"]);
 console.log(person["address"]["streetName"]);
 console.log(person.address.flatNumber);
-console.log(person.role[1])
+console.log(person.role[1]);
 
 let basketData = [
   { namePerson: "Joseph", item: "T-shirt", price: 30 },
@@ -31,3 +31,102 @@ let basketData = [
 ];
 
 console.log(basketData[1]["price"]);
+
+// let obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+let key = "a";
+
+// let obj = {
+//   [key]: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+let obj = {
+  [key + "1"]: 1,
+  [key + "2"]: 2,
+  [key + "3"]: 3,
+  display() {
+    console.log(obj[key]);
+  },
+};
+
+obj.display();
+
+// Exercise
+let obj1 = {
+  x: 1,
+  y: 2,
+  z: 3,
+  square() {
+    console.log("The square of", this.x, "is", this.x * this.x);
+    console.log("The square of", this.y, "is", this.y * this.y);
+    console.log("The square of", this.z, "is", this.z * this.z);
+  },
+};
+obj1.square();
+
+// Exercise
+let obj2 = {
+  "1a": 1,
+  b2: 2,
+  "с-с": 3,
+  "d 4": 4,
+  e5: 5,
+  answers() {
+    console.log("'1a'", "requires quotes");
+    console.log("'b2'", "does not require quotes");
+    console.log("'c-c'", "requires quotes");
+    console.log("'d 4'", "requires quotes");
+    console.log("'e5'", "does not require quotes");
+  },
+};
+obj2.answers();
+
+// Exercise
+let obj3 = {
+  "1a": 1,
+  b2: 2,
+  "c-c": 3,
+  "d 4": 4,
+  e5: 5,
+};
+
+console.log(obj3["1a"]);
+console.log(obj3.b2);
+console.log(obj3["c-c"]);
+console.log(obj3["d 4"]);
+console.log(obj3.e5);
+
+// Exercise
+let key4 = "x";
+
+let obj4 = {
+  [key4]: 1,
+  y: 2,
+  z: 3,
+};
+
+// Exercise - need to check
+let obj5 = {
+  x: 1,
+  y: 2,
+  z: 3,
+};
+
+let key1 = "x";
+let key2 = "y";
+let key3 = "z";
+
+// Exercise
+let obj6 = { x: 1, y: 2, z: 3 };
+console.log(obj6["x"]);
+
+// Exercise
+let obj7 = { x: 1, y: 2, z: 3 };
+let key7 = "x";
+console.log(obj7[key7]);
