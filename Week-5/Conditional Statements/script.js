2091bb74b454b4abffb3f680591d29e08e6dc8cd
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-17 17:10:33 +0000

    Conditional Statements

---
+++ Week-5/Conditional Statements/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let day = "Monday";
+
+if(day === "Monday"){
+    console.log("Today is Monday");
+}
+else if(day === "Tuesday"){
+    console.log("Today is not Monday");
+}
+else if(day === "Wednesday"){
+    console.log("Today is Wednesday");
+}
+else{
+    console.log("None of the above is true");
+};
