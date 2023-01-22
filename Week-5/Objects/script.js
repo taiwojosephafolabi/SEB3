783dcae8c853caf3b23422de2e2c23d0ebcf201f
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-22 17:20:09 +0000

    Objects

--- Week-5/Objects/script.js
+++ Week-5/Objects/script.js
@@ -130,3 +130,76 @@ console.log(obj6["x"]);
 let obj7 = { x: 1, y: 2, z: 3 };
 let key7 = "x";
 console.log(obj7[key7]);
+
+// Exercise
+let obj8 = {
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+  7: "Sunday",
+};
+
+console.log(obj8[1], obj8[2], obj8[3], obj8[4], obj8[5], obj8[6], obj8[7]);
+
+// Exercise
+let obj9 = { 23: "Sun", 1: "Moon", 46: "Stars" };
+console.log(obj9[46], obj9[23], obj9[1]);
+
+// Object Method - start of Wed 18th lesson
+
+const myBuilding = {
+  name: "Tower Bridge",
+  color: "blue",
+  callTheName: function (location) {
+    return "This is", location;
+  },
+};
+
+console.log(myBuilding["color"]);
+console.log(myBuilding.callTheName("London Eye"));
+
+// remove
+delete myBuilding.name;
+console.log(myBuilding.name);
+console.log(myBuilding);
+
+// add again - do again
+
+myBuilding["name"] = "London Eye";
+console.log(myBuilding.name);
+
+// Object.keys()
+
+Object.keys(myBuilding);
+console.log(Object.keys(myBuilding));
+
+for (let keyName in myBuilding) {
+  console.log(keyName);
+}
+
+// Exercise
+
+let obj10 = { x: 1, y: 2, z: 3 };
+let keyArr = Object.keys(obj10);
+console.log(keyArr);
+
+// Exercise
+let obj11 = { x: 1, y: 2, z: 3 };
+console.log(Object.keys(obj10).length);
+
+// Break and continue
+
+for(let i = 0; i < 10; i++){
+  if(i > 1 && i < 5){
+    console.log(i)
+  } 
+  else if(i >= 5 && i < 7){
+    continue
+  } 
+  else{
+    break
+  }
+};
