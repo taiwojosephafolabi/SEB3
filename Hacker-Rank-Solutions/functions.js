7a1604539cc63f4354947f4dd2d612df586a349f
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-02-07 19:40:45 +0000

    new functions

--- Hacker-Rank-Solutions/functions.js
+++ Hacker-Rank-Solutions/functions.js
@@ -1,5 +1,9 @@
-function factorial (n) {
-  for (var i = n-1; n>=1 ; i--) {
-    n *= i;
-    return n;
+function factorial(integer) {
+  if (integer === 0 || integer === 1) {
+    return 1;
+  }
+  for (let num = integer - 1; num >= 1; num--) {
+    integer *= num;
+    return integer;
+  }
 };
