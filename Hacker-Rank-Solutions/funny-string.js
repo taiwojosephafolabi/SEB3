217ac254694afc9f6c6b764aea065e9692a4e785
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-02-12 23:24:08 +0000

    funny string final solution

--- Hacker-Rank-Solutions/funny-string.js
+++ Hacker-Rank-Solutions/funny-string.js
@@ -10,4 +10,17 @@ function funnyString(s) {
       return "Not Funny";
     }
   }
+};
+
+// Final solution
+function funnyString(s) {
+  let r = s.split("").reverse().join("");
+  for (let i=0; i < s.length - 1; i++){
+      let sValueDif = Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1));
+      let rValueDif = Math.abs(r.charCodeAt(i) - r.charCodeAt(i+1));
+      if(sValueDif !== rValueDif){
+          return "Not Funny"
+      };
+  };
+  return "Funny"
 }
