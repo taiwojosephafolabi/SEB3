02cda7cfc900b369b55aa1173c5760e3063d8e35
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-02-12 19:45:42 +0000

    birthday cake candles final solution

--- Hacker-Rank-Solutions/birthday-cake-candles.js
+++ Hacker-Rank-Solutions/birthday-cake-candles.js
@@ -1,11 +1,26 @@
+// 1st attempt
 function birthdayCakeCandles(candles) {
-  // Write your code here
   let sortedCandles = candles.sort(function(a, b){return b-a});
   let numCandles = 1;
   for (let i = 0; i < candles.length; i++){
       if(sortedCandles[i] == sortedCandles[(i+1)]){
           numCandles += 1;
   }
+  }
+  return numCandles;
+}
+// Final solution
+function birthdayCakeCandles(candles) {
+  let sortedCandles = candles.sort(function(a, b){return b-a});
+  let numCandles = 1;
+  let i = 0;
+  while (i < candles.length) {
+      if(candles[i] === candles[i+1]){
+          numCandles++;
+          i++
+      } else {
+          break
+      }
   }
   return numCandles;
 }
