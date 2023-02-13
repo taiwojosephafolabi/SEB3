927c897617e69e8a6ce3119c59bae19f4cefe522
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-02-13 00:26:56 +0000

    training js 11 final solution

--- Codewars-Solutions/training-js-11.js
+++ Codewars-Solutions/training-js-11.js
@@ -1,7 +1,6 @@
 // First attempt 
 function grabDoll(dolls) {
   var bag = [];
-  //coding here
   let count = 0;
   for (let i = 0; i < dolls.length; i++) {
     if (dolls[i] == "Hello Kitty" || dolls[i] == "Barbie doll") {
@@ -11,6 +10,29 @@ function grabDoll(dolls) {
       break;
     } else {
       continue;
+    }
+  }
+  return bag;
+}
+
+// Final solution
+function grabDoll(dolls){
+  var bag=[];
+  let count = 0;
+  for(let i = 0; i < dolls.length; i++){
+    if (dolls[i] === "Hello Kitty" && count < 3){
+      bag.push(dolls[i]);
+      count+=1
+    }
+    else if (dolls[i] === "Barbie doll" && count < 3){
+      bag.push(dolls[i]);
+      count+=1
+    }
+    else if (dolls[i] !== "Hello Kitty" || dolls[i] !== "Barbie doll"){
+      continue
+    }
+    else{
+      break 
     }
   }
   return bag;
