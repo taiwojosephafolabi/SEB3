b24bc7bb2c940f0c6743c5bf23485a80de8f9597
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-02-27 09:14:07 +0000

    one promise test added

--- Week-10/Homework - Jest/math.js
+++ Week-10/Homework - Jest/math.js
@@ -15,17 +15,92 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
-const agePromise = new Promise((resolve, reject) => {
-  resolve(subtract(num1, num2));
-  reject(console.log("Error"))
-});
-
-agePromise
-  .then((num1, num2) => {
-    age;
+function fetchAge(){
+  return new Promise((resolve, reject) => {
+    resolve(subtract(2023, 2021));
+    reject("Error fetching age")
   })
-  .catch((err) => {
-    console.log("check calculation");
-  });
+};
 
-module.exports = { sum, subtract, multiply, divide, agePromise };
+
+
+
+
+
+
+
+
+// function UserRequest(year1, year2) {
+//   return new Promise((resolve, reject) => {
+//     console.log("Calculating your age:" + subtract(year1, year2))
+//     if (subtract(year1, year2) === 21) {
+//       resolve("Your age is: 21");
+//     } else {
+//       reject ("You are not the user")
+//     }
+//   })
+// }
+
+// const agePromise = new Promise((resolve, reject) => {
+//   let num1 = 2023;
+//   let num2 = 2021;
+//   resolve(subtract(num1, num2));
+//   reject(console.log("Error"))
+// });
+
+// function processRequest(year1, year2) {
+//   return new Promise((resolve,reject) => {
+//     console.log("Processing response")
+//     resolve("Calculting age")
+//   })
+// };
+
+// async function doWork() {
+//   let response = await UserRequest("21");
+//   console.log("Response Received");
+//   let processedResponse = await processRequest(response);
+//   console.log(processedResponse)
+// };
+
+// makeRequest("21")
+//     .then((response) => {
+//       console.log("Response Received");
+//       return processRequest(response);
+//     })
+//     .then((processedResponse) => {
+//       console.log(processedResponse);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+// doWork();
+
+
+
+// // agePromise
+// //   .then((num1, num2) => {
+// //     age;
+// //   })
+// //   .catch((err) => {
+// //     console.log("check calculation");
+// //   });
+
+
+
+// // async function askFavColour() {
+// //   try {
+// //     const response = await user("Joseph");
+// //     console.log("Response Received");
+// //     const favColour = await favouriteColour(response);
+// //     console.log(favColour);
+// //     // const favNumber = await favouritenumber(response);
+// //     // console.log(favNumber)
+// //   } catch (err) {
+// //     console.log(err)
+// //   }
+// // };
+
+
+
+module.exports = { sum, subtract, multiply, divide, fetchAge, /*agePromise*/ };
--- Week-10/Homework - Jest/math.test.js
+++ Week-10/Homework - Jest/math.test.js
@@ -1,18 +1,24 @@
-"use strict"
-const { sum, multiply, subtract, divide, agePromise} = require("./math");
+"use strict";
+const {
+  sum,
+  multiply,
+  subtract,
+  divide,
+  fetchAge /*agePromise*/,
+} = require("./math");
 
 describe("Testing math functions", () => {
   test("Sum: Adding 3 + 2 equals 5", () => {
     expect(sum(3, 2)).toBe(5);
   });
 
-  test("Sum: Adding 7 + 2 equals 5", () =>{
+  test("Sum: Adding 7 + 2 equals 5", () => {
     expect(sum(7, 2)).not.toBe(5);
   });
 
   test("Sum is called with 2 arguments", () => {
-    const num = {num1: 3, num2: 2};
-    const sumMock = jest.fn(num => num.num1, num.num2);
+    const num = { num1: 3, num2: 2 };
+    const sumMock = jest.fn((num) => num.num1, num.num2);
     sumMock(num);
     expect(sumMock).toHaveReturnedWith(3, 2);
   });
@@ -38,27 +44,41 @@ describe("Testing math functions", () => {
   });
 
   test("Divide: 10 / 3 close to 3.3", () => {
-    expect(divide(10, 3)).toBeCloseTo(3.3, 1)
+    expect(divide(10, 3)).toBeCloseTo(3.3, 1);
   });
 
   test("Divide: 50 / 4 greater than 10", () => {
-    expect(divide(50, 4)).toBeGreaterThan(10)
+    expect(divide(50, 4)).toBeGreaterThan(10);
   });
 });
 
 describe("the arguments return age", () => {
-  test("the arguments return age", async () => {
-    const data = await agePromise(2023, 2001).then(age => {
-      expect(data).resolves.toBe(21);
-    });
+  test("Promise testing", () => {
+    return fetchAge()
+      .then((data) => {
+        expect(data).toBe(21);
+      })
+      .catch((err) => {
+        console.log("Wait for your birthday");
+      });
   });
 
 
+
+
+
+  // test("the arguments return age", async () => {
+  //   const data = await agePromise(2023, 2001).then(age => {
+  //     expect(data).resolves.toBe(21);
+  //   });
+  // });
+
+  // test("the function fails with an error", async () => {
+  //   expect.assertions(1);
+  //   try {
+  //     await agePromise(2023, 2001);
+  //   } catch (err) {
+  //     expect(err).toMatch("error");
+  //   }
+  // });
 });
-
-
-
-
-
-
-
