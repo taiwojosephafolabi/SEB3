c049a763875b066b27117e5a8fc19bfa0c9535c7
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-03-06 18:04:24 +0000

    Debugging

---
+++ Week-11/Debugging/script.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 20;
+
+let z = x * y;
+
+
+let scores = [0, 12, 20, 19, 19, 11, 15 ,17];
+
+let result = scores.push(13)
+
+let result2 = scores.join(2)
+
+
+let fruits = ["apple", "orange", "pineapple", "mango", "cherries", "bananas"];
+
+let result3 = fruits.map((fruit) =>  fruit + "?")
+
+debugger;
